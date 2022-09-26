eb74ad5e5a083c7d93ab5e94c5a04e40c17f8208
Harman9a <Harman9a@gmail.com>
2022-09-26 15:26:01 +0530

    added login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import { IonContent, IonPage } from "@ionic/react";
+
+const Login: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <div>Login</div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
